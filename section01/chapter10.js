e860baa9a815ebb6a92e9dacb9d97b00d69f8b5c
kkhhjjoo <khjoo1203@gmail.com>
2025-09-27 16:40:11 +0900

    반복문

---
+++ section01/chapter10.js
@@ -0,0 +1,16 @@
+//for문
+//for (초기식; 조건식; 증감식) { 
+//  console.log("반복!");
+//}
+
+for (let idx = 1; idx <= 10; idx++) { 
+  if (idx % 2 === 0) { 
+    continue;
+  }
+
+  console.log(idx);
+
+  if (idx >= 5) { 
+    break;
+  }
+}
