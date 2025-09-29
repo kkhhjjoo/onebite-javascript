797a4e115111692f8f613cdac4ed353d29a706e6
kkhhjjoo <khjoo1203@gmail.com>
2025-09-29 22:04:48 +0900

    Spread 연산자와 Rest 매개변수

---
+++ section02/chapter04.js
@@ -0,0 +1,14 @@
+//1. Spread연산자
+//Spread : 흩뿌리다, 펼치다 라는 뜻
+//객체나 배열에 저장된 여러개의 값을 개별로 흩뿌려주는 역할
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+
+//2. Rest 매개변수
+//Rest는 나머지, 나머지 매개변수
+function funcB(...rest) { 
+  console.log(rest);
+}
+
+funcB(...arr1);
