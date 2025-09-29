70d527cb016b60ba62124f5c3dd279e2aba8f450
kkhhjjoo <khjoo1203@gmail.com>
2025-09-29 17:55:43 +0900

    단락 평가

---
+++ section02/chapter02.js
@@ -0,0 +1,24 @@
+// function returnFalse() {
+//   console.log("False 함수");
+//   return undefined;
+// }
+
+// function returnTrue() {
+//   console.log("True 함수");
+//   return 10;
+// }
+
+// console.log(returnFalse() && returnTrue());
+
+//단락 평가 활용 사례
+
+function printName(person) { 
+  // if (!person) {
+  //   console.log('person에 값이 없음');
+  //   return;
+  // }
+  const name = person && person.name
+  console.log(name || 'person의 값이 없음');
+}
+printName();
+printName({ name: '이정환' });
