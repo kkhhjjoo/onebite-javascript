c1ecc78877b87ad3d5fa27cea84021fd944ebd81
kkhhjjoo <khjoo1203@gmail.com>
2025-12-06 21:27:47 +0900

    Node.js 모듈 시스템 이해하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,12 @@
-console.log('안녕 Node.js');
+// const moduleData = require('./math');
+import mul from './math.js';
+import { add, sub } from './math.js';
+// console.log(moduleData.add(1, 2));
+console.log(add(1, 2));
+
+// console.log(moduleData.sub(1, 2));
+console.log(sub(1, 2));
+// console.log(moduleData);
+// console.log('안녕 Node.js');
+
+console.log(mul(3, 2));
