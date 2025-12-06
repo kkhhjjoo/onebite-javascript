46a59a8e873cfbeb6485bb8651bdc219f94c8c8e
kkhhjjoo <khjoo1203@gmail.com>
2025-12-06 21:38:36 +0900

    Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,12 +1,17 @@
-// const moduleData = require('./math');
-import mul from './math.js';
-import { add, sub } from './math.js';
-// console.log(moduleData.add(1, 2));
-console.log(add(1, 2));
+// // const moduleData = require('./math');
+// import mul from './math.js';
+// import { add, sub } from './math.js';
+// // console.log(moduleData.add(1, 2));
+// console.log(add(1, 2));
 
-// console.log(moduleData.sub(1, 2));
-console.log(sub(1, 2));
-// console.log(moduleData);
-// console.log('안녕 Node.js');
+// // console.log(moduleData.sub(1, 2));
+// console.log(sub(1, 2));
+// // console.log(moduleData);
+// // console.log('안녕 Node.js');
 
-console.log(mul(3, 2));
+// console.log(mul(3, 2));
+
+import randomColor from 'randomcolor';
+
+const color = randomColor();
+console.log(color);
